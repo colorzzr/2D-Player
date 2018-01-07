d2c0502cb3c9d8411002ae17c4d06a74f21bdae7
colorzzr <colorzzr@gmail.com>
2018-01-06 21:24:11 -0500

    change alll to the enumerical

---
+++ Assets/PlayMaker Custom Scripts/My Game/HeroAct.cs
@@ -0,0 +1,18 @@
+// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+// THIS CONTENT IS AUTOMATICALLY GENERATED.
+// [PLAYMAKER_ENUM]
+// It is recommended to use PlayMaker Enum Creator Wizard to edit this enum definition. But you can perfectly edit this script directly
+
+namespace Com.MyGame
+{
+	public enum HeroAct
+	{
+		Attack,
+		Fire,
+		Ice,
+		Poison,
+		Stun,
+		KickOff,
+		Nothing		
+	}
+}
---
+++ Assets/PlayMaker Custom Scripts/My Game/specialEff.cs
@@ -0,0 +1,18 @@
+// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+// THIS CONTENT IS AUTOMATICALLY GENERATED.
+// [PLAYMAKER_ENUM]
+// It is recommended to use PlayMaker Enum Creator Wizard to edit this enum definition. But you can perfectly edit this script directly
+
+namespace Com.MyGame
+{
+	public enum SpecialEff
+	{
+		bleeding,
+		iced,
+		firing,
+		kickOff,
+		nothing,
+		poison,
+		stun		
+	}
+}
